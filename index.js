a9f78411e59579a774be3ea33a4eef3e5a791a7f
Michal Skwara <michal.skwara@fingoweb.com>
2022-01-21 23:47:56 +0100

    working

--- index.js
+++ index.js
@@ -1,40 +1,83 @@
 const fs = require("fs");
 const ytdl = require("ytdl-core");
-const ffmpeg = require('fluent-ffmpeg');
+const ffmpeg = require("fluent-ffmpeg");
 const data = require("./data");
 
-ffmpeg.setFfmpegPath('./node_modules/ffmpeg/lib');
-ffmpeg.setFfprobePath('./node_modules/ffprobe');
+const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
+ffmpeg.setFfmpegPath(ffmpegPath);
 
-ffmpeg('./output/Sea Shanties - vol1.mp3')
-      .inputOptions('-t 5') // 2s
-      .output('output.mp3')
-      .run()
+if (!fs.existsSync("./output")) {
+  fs.mkdirSync("./output");
+}
 
-// if (!fs.existsSync('./output')) {
-//   fs.mkdirSync('./output');
-// }
+if (!fs.existsSync("./output/final")) {
+  fs.mkdirSync("./output/final");
+}
 
-// for (const entry of data) {
-//   ytdl.getInfo(entry.url).then((info) => {
-//     const readableStream = ytdl(entry.url, {
-//       format: ytdl.chooseFormat(info.formats, { quality: "140" }),
-//     });
+if (!fs.existsSync("./output/temp")) {
+  fs.mkdirSync("./output/temp");
+}
 
-//     readableStream.on("pipe", () => {
-//       console.log("Processing: ", entry.title);
-//     });
+const trim = (src, start, end, title, output) => {
+  return new Promise((resolve, reject) => {
+    let order = ffmpeg(src)
+      .output(output)
+      .on("end", function (err) {
+        if (!err) {
+          console.log(`Zapisano: ${title}`);
+          resolve();
+        }
+      })
+      .on("error", function (err) {
+        console.log(`Error: ${err}`);
+        reject();
+      });
+    if (start != null) {
+      order = order.setStartTime(start);
+    }
+    if (start != null && end != null) {
+      order = order.setDuration(end - start);
+    }
+    order.run();
+  });
+};
 
-    
-//     readableStream.on("end", () => {
-//       console.log("File saved: ", entry.title);
+for (const entry of data) {
+  const fullAudioPath = `./output/temp/${entry.title}.mp3`;
+  ytdl.getInfo(entry.url).then((info) => {
+    const readableStream = ytdl(entry.url, {
+      format: ytdl.chooseFormat(info.formats, { quality: "140" }),
+    });
 
-//       ffmpeg(`./output/${entry.title}.mp3`)
-//       .inputOptions('-t 5') // 2s
-//       .output('output.mp3')
-//       .run()
-//     });
+    readableStream.on("pipe", () => {
+      console.log("Processing: ", entry.title);
+    });
 
-//     readableStream.pipe(fs.createWriteStream(`./output/${entry.title}.mp3`));
-//   });
-// }
+    readableStream.on("end", async () => {
+      console.log("File saved: ", entry.title);
+
+      const packagePath = `./output/final/${entry.title}`;
+      if (!fs.existsSync(packagePath)) {
+        fs.mkdirSync(packagePath);
+      }
+      for (let i = 0; i < info.videoDetails.chapters.length; i++) {
+        const currentChapter = info.videoDetails.chapters[i];
+        const nextChapter = info.videoDetails.chapters[i + 1];
+        const start = currentChapter.start_time;
+        const end = nextChapter ? nextChapter.start_time : null;
+
+        const chapterPath = `./output/final/${entry.title}/${currentChapter.title}.mp3`;
+        console.log({ start, end });
+        await trim(
+          fullAudioPath,
+          start,
+          end,
+          currentChapter.title,
+          chapterPath
+        );
+      }
+    });
+
+    readableStream.pipe(fs.createWriteStream(fullAudioPath));
+  });
+}
