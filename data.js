fbeeb6dcb6d6d7ec88e14e3dce311c02b47a7142
Michał Skwara <michal.skwara@stxnext.pl>
2022-07-11 17:37:12 +0200

    playlist downloading support

--- data.js
+++ data.js
@@ -15,68 +15,6 @@ module.exports = [
     splitByChapters: true,
   },
   {
-    url: "https://www.youtube.com/watch?v=h0sdHq20ILM",
-    title: "Sea Shanties - vol4",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=7kT33murB3c",
-    title: "Sea Shanties - vol5",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=I8ehKqcKtQ0",
-    title: "Sea Shanties - vol6",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=ruihvv3k_y8",
-    title: "Sea Shanties - vol7",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=Tai2WC5BB_c",
-    title: "Sea Shanties - vol8",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=mPXxBJyxg18",
-    title: "Sea Shanties - vol9",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=C_DeVV-ai8Y",
-    title: "Sea Shanties - vol10",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=hBFt9d164lk",
-    title: "Sea Shanties - vol11",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=EXio81PV8WI",
-    title: "Sea Shanties - vol12",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=BQHBVKklpsQ",
-    title: "Sea Shanties - vol13",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=0ARg3HbQT80",
-    title: "Sea Shanties - vol14",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=k1_5YZS_H80",
-    title: "Sea Shanties - vol15",
-    splitByChapters: true,
-  },
-  {
-    url: "https://www.youtube.com/watch?v=jZ07wZW5lmg",
-    title: "Sea Shanties - vol16",
-    splitByChapters: true,
+    url: "https://www.youtube.com/playlist?list=PLbd8mEjGKXNSYG5aZX9ODmZWvOavycY2O",
   },
 ];
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const ffmpeg = require('fluent-ffmpeg');
+const { scrapePlaylist } = require('youtube-playlist-scraper');
 const _uniqueId = require('lodash/uniqueId');
 const _values = require('lodash/values');
 const _every = require('lodash/every');
+const _includes = require('lodash/includes');
+const _split = require('lodash/split');
+const _map = require('lodash/map');
 
 const createDirectory = require('./utils/createDirectory');
 const trim = require('./utils/trim');
@@ -17,66 +21,117 @@ createDirectory('./output');
 createDirectory('./output/final');
 createDirectory('./output/temp');
 
-const isDone = {};
+const playlistSubstringIdentifier = 'playlist?list=';
+
+var isDone = {};
 
 const onProgramEnd = () => {
-  if(_every(_values(isDone), value => value === true)) {
+  if (_every(_values(isDone), (value) => value === true)) {
     console.log('Cleaning up...');
     fs.rmSync('./output/temp', { recursive: true, force: true });
-    console.log('\n ===> Remember to copy all the files in /output/final directory to a safe place! <=== \n');
+    console.log(
+      '\n ===> Remember to copy all the files in /output/final directory to a safe place! <=== \n'
+    );
   }
-}
+};
 
-for (const entry of data) {
-  let started = false;
-  const entryHash = _uniqueId('entry');
-  isDone[entryHash] = false;
+const downloadRequestedMusic = async () => {
+  const preparedData = [];
 
-  ytdl.getInfo(entry.url).then((info) => {
-    const validTitle = getValidFilename(entry.title || info.videoDetails.title);
-    const fullAudioPath = `./output/temp/${validTitle}.mp3`;
-    const readableStream = ytdl(entry.url, {
-      format: ytdl.chooseFormat(info.formats, { quality: '140' }),
-    });
+  for (const entry of data) {
+    if (_includes(entry.url, playlistSubstringIdentifier)) {
+      const playlistId = _split(entry.url, playlistSubstringIdentifier)[1];
+      const { title: playlistTitle, playlist: playlistData } =
+        await scrapePlaylist(playlistId);
+      preparedData.push(
+        ..._map(playlistData, (obj) => ({
+          url: obj.url,
+          title: '',
+          splitByChapters: false,
+          isFromPlaylist: true,
+          playlistTitle,
+        }))
+      );
+    } else {
+      preparedData.push(entry);
+    }
+  }
 
-    readableStream.on('pipe', () => {
-      if (!started) {
-        console.log(`Downloading: ${validTitle}...`);
-        started = true;
-      }
-    });
+  for (const entry of preparedData) {
+    let started = false;
+    const entryHash = _uniqueId('entry');
+    isDone[entryHash] = false;
 
-    readableStream.on('end', async () => {
-      console.log(`Successfully saved URL audio: ${validTitle}.`);
+    ytdl.getInfo(entry.url).then((info) => {
+      const validTitle = getValidFilename(
+        entry.title || info.videoDetails.title
+      );
+      const fullAudioPath = `./output/temp/${validTitle}.mp3`;
+      const readableStream = ytdl(entry.url, {
+        format: ytdl.chooseFormat(info.formats, { quality: '140' }),
+      });
 
-      if(!entry.splitByChapters) {
-        const finalOutput = `./output/final/${validTitle}.mp3`;
-        trim(fullAudioPath, null, null, validTitle, finalOutput).finally(() => {
+      readableStream.on('pipe', () => {
+        if (!started) {
+          console.log(`Downloading: ${validTitle}...`);
+          started = true;
+        }
+      });
+
+      readableStream.on('end', async () => {
+        console.log(`Successfully saved URL audio: ${validTitle}.`);
+
+        if (entry.isFromPlaylist) {
+          const packagePath = `./output/final/${entry.playlistTitle}`;
+          createDirectory(packagePath);
+          const finalOutput = `${packagePath}/${validTitle}.mp3`;
+          trim(fullAudioPath, null, null, validTitle, finalOutput).finally(
+            () => {
+              isDone[entryHash] = true;
+              onProgramEnd();
+            }
+          );
+          return;
+        }
+
+        if (!entry.splitByChapters) {
+          const finalOutput = `./output/final/${validTitle}.mp3`;
+          trim(fullAudioPath, null, null, validTitle, finalOutput).finally(
+            () => {
+              isDone[entryHash] = true;
+              onProgramEnd();
+            }
+          );
+          return;
+        }
+
+        const packagePath = `./output/final/${validTitle}`;
+        createDirectory(packagePath);
+
+        const chapterPromises = [];
+
+        for (let i = 0; i < info.videoDetails.chapters.length; i++) {
+          const currentChapter = info.videoDetails.chapters[i];
+          const nextChapter = info.videoDetails.chapters[i + 1];
+          const start = currentChapter.start_time;
+          const end = nextChapter ? nextChapter.start_time : null;
+
+          const chapterTitle = getValidFilename(currentChapter.title);
+          const chapterPath = `${packagePath}/${chapterTitle}.mp3`;
+
+          chapterPromises.push(
+            trim(fullAudioPath, start, end, chapterTitle, chapterPath)
+          );
+        }
+        Promise.all(chapterPromises).finally(() => {
           isDone[entryHash] = true;
           onProgramEnd();
         });
-        return;
-      }
+      });
 
-      const packagePath = `./output/final/${validTitle}`;
-      createDirectory(packagePath);
-
-      for (let i = 0; i < info.videoDetails.chapters.length; i++) {
-        const currentChapter = info.videoDetails.chapters[i];
-        const nextChapter = info.videoDetails.chapters[i + 1];
-        const start = currentChapter.start_time;
-        const end = nextChapter ? nextChapter.start_time : null;
-
-        const chapterTitle = getValidFilename(currentChapter.title);
-        const chapterPath = `${packagePath}/${chapterTitle}.mp3`;
-
-        trim(fullAudioPath, start, end, chapterTitle, chapterPath).finally(() => {
-          isDone[entryHash] = true;
-          onProgramEnd();
-        });
-      }
+      readableStream.pipe(fs.createWriteStream(fullAudioPath));
     });
+  }
+};
 
-    readableStream.pipe(fs.createWriteStream(fullAudioPath));
-  });
-}
+downloadRequestedMusic();
